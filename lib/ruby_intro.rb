8fb4cf97c040241681859eef23ccdc1fdfd4972f
elizabethchristman <ubuntu@ip-172-31-40-150.ec2.internal>
2020-01-03 10:42:47 +0000

    hw-ruby-intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,71 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  #arr.sum
+  result = 0
+  arr.each {|x| result = result + x}
+  result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  #arr.sort!
+  #result = 0
+  #result = arr.last(2).sum
+  #result
+  arr.max(2).sum
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? {|x,y| x + y == n}
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, "
+  str.concat(name)
+  str
+
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ Regexp.new('^[qwrtypsdfghjklzxcvbnm]', Regexp::IGNORECASE).match?(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^(0|00|([01]+00))$/.match?(s)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    
+    if @price <= 0 
+      raise ArgumentError.new("price must be greater than 0")
+    end
+    if @isbn == ''
+      raise ArgumentError.new("isbn cannot be an empty string")
+    end
+  end
+  def isbn
+    @isbn
+  end
+  def price
+    @price
+  end
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  def price=(price)
+    @price = price
+  end
+  
+  def price_as_string
+    return "$%.2f" % price
+  end
 end
